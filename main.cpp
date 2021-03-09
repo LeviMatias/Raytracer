cb0bab7ecda36167930f6689ff1be54f8d5e5928
LeviMatias <matilevi35@gmail.com>
2021-03-09 16:54:56 -0300

    using rays and setting blue background color blend

--- main.cpp
+++ main.cpp
@@ -7,22 +7,38 @@
 
 
 int main() {
-    const int img_w = 256;
-    const int img_h = 256;
+    //image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int img_w = 400;
+    const int img_h = static_cast<int>(img_w / aspect_ratio);
 
-    //add image header
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
     SafeStream out;
     out.OpenWrite("image.ppm");
 
+    //add image header
     out << "P3\n" << img_w << ' ' << img_h << "\n255\n";;
 
     for (short int j = img_h-1; j >= 0; j--){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (short int i = 0; i < img_w; i++){
-            color pixel_color(double(i) / (img_w - 1), double(j) / (img_h - 1), 0.25);
+            auto u = double(i) / (img_w-1);
+            auto v = double(j) / (img_h-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+
+            color pixel_color = ray_color(r);
             write_color(out, pixel_color);
         }
     }
-
+    std::cerr << "\nDone.\n";
     return 0;
 }
--- utility/color_helper.h
+++ utility/color_helper.h
@@ -9,6 +9,9 @@
 #include "safe_stream.h"
 #include "ray.h"
 
+#define LIGHT_BLUE color(0.5, 0.7, 1.0)
+#define WHITE color(1.0, 1.0, 1.0)
+
 void write_color(SafeStream &out, color pixel_color) {
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
@@ -20,7 +23,7 @@ color ray_color(const ray &r){
     //gradient background color
     vec3 unit_direction = r.dir.unit();
     double t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    return (1.0-t)*WHITE + t*LIGHT_BLUE;
 }
 
 #endif
