e407c818a060455825175b06a2fa3906d23eb343
LeviMatias <matilevi35@gmail.com>
2021-03-12 17:45:11 -0300

    possible painter thread implementation

--- model/painter_thread.cpp
+++ model/painter_thread.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "painter_thread.h"
+#include "../utility/Ray.h"
+
+void PainterThread::_run() {
+    for (int j = canvas->h-1; j >= 0; j-= offset){
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < canvas->w; i+= offset){
+            Color pixel_color(0, 0, 0);
+            for (int s = 0; s < STROKES; ++s) {
+                auto u = (i + Random::NextNumber()) / (canvas->w-1);
+                auto v = (j + Random::NextNumber()) / (canvas->h-1);
+                Ray r = cam->GetRay(u, v);
+                pixel_color += Ray2Color(r);
+            }
+            canvas->WriteColor(pixel_color/STROKES);
+        }
+    }
+}
+
+Color PainterThread::Ray2Color(const Ray& r) const {
+    hit_record rec;
+    if (world->hit(r, 0, INF, rec)) {
+        return 0.5 * (rec.normal + Color(1, 1, 1));
+    }
+    Vec3 unit_direction = r.direction.unit();
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
--- model/painter_thread.h
+++ model/painter_thread.h
@@ -7,16 +7,29 @@
 
 
 #include "../utility/BaseThread.h"
+#include "../utility/Random.h"
+#include "../utility/Utility.h"
 #include "Image.h"
+#include "Camera.h"
+#include "Scene.h"
+
+#define STROKES 100
+#define LIGHT_BLUE color(0.5, 0.7, 1.0)
+#define WHITE color(1.0, 1.0, 1.0)
 
 class PainterThread : public BaseThread {
 public:
+    Hittable* world;
+    Camera* cam;
     Image * canvas;
+    int offset;
 
     // PainterThread(Image* img);
 
 private:
-    // void _run() override;
+    void _run() override;
+
+    Color Ray2Color(const Ray& r) const;
 };
 
 
