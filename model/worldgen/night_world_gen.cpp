771a52d1c537e3c3e5327ebb1f9e2ae0f519f61a
LeviMatias <matilevi35@gmail.com>
2021-04-21 17:54:11 -0300

    world domination

---
+++ model/worldgen/night_world_gen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matias Ignacio Levi Fernandez on 21/04/2021.
+//
+
+#include "night_world_gen.h"
---
+++ model/worldgen/night_world_gen.h
@@ -0,0 +1,14 @@
+//
+// Created by Matias Ignacio Levi Fernandez on 21/04/2021.
+//
+
+#ifndef RAYTRACER_NIGHT_WORLD_GEN_H
+#define RAYTRACER_NIGHT_WORLD_GEN_H
+
+
+class NightWorldGen {
+
+};
+
+
+#endif //RAYTRACER_NIGHT_WORLD_GEN_H
