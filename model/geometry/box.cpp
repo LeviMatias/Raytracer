45c6fc388126413b594e5cd1548a77687e8aaf1a
LeviMatias <matilevi35@gmail.com>
2021-05-14 12:34:08 -0300

    debug color fix

--- model/geometry/box.cpp
+++ model/geometry/box.cpp
@@ -19,11 +19,11 @@ Box::Box(Point3 p0, Point3 p1, std::shared_ptr<Material> &m) : Hittable(m, p0, p
     v.push_back(std::make_shared<XYRect>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z() ,m1));
     v.push_back(std::make_shared<XYRect>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z() ,m1));
 
-    v.push_back(std::make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x() ,m3));
-    v.push_back(std::make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x() ,m3));
-
     v.push_back(std::make_shared<XZRect>(p0.x(), p1.x(), p0.z(), p1.z(), p0.y() ,m2));
     v.push_back(std::make_shared<XZRect>(p0.x(), p1.x(), p0.z(), p1.z(), p1.y() ,m2));
+
+    v.push_back(std::make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x() ,m3));
+    v.push_back(std::make_shared<YZRect>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x() ,m3));
 
     sides = std::make_shared<BVH_Node>(v);
 }
@@ -31,7 +31,7 @@ Box::Box(Point3 p0, Point3 p1, std::shared_ptr<Material> &m) : Hittable(m, p0, p
 bool Box::Hit(const Ray &r, double t_min, double t_max, hit_record &rec) const {
     bool h = false;
     for (auto &e : v){
-        h = e->Hit(r, t_min, t_max, rec) || h;
+        h = e->Hit(r, t_min, (h) ? rec.t : t_max, rec) || h;
     }
     return h;
 }
--- model/scene.cpp
+++ model/scene.cpp
@@ -7,7 +7,7 @@
 bool Scene::Hit(const Ray &r, double t_min, double t_max, hit_record &rec) const {
     bool h = false;
     for (auto &e : *a){
-        h = e->Hit(r, t_min, t_max, rec) || h;
+        h = e->Hit(r, t_min, (h) ? rec.t : t_max, rec) || h;
     }
     return h;
 }
